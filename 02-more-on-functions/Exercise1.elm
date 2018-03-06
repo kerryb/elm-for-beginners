71aea6e1c3bf0129103f4877f6dfce92360113da
Kerry Buckley <kerryjbuckley@gmail.com>
2018-03-06 11:08:02 +0000

    Exercise 2.1: infix function

---
+++ 02-more-on-functions/Exercise1.elm
@@ -0,0 +1,16 @@
+module Main exposing (..)
+
+import Html exposing (text)
+
+
+(~=) str1 str2 =
+    (String.left 1 str1) == (String.left 1 str2)
+
+
+main =
+    Html.text
+        ("Foo, Bar: "
+            ++ (toString ("Foo" ~= "Bar"))
+            ++ "; Foo, Frog: "
+            ++ (toString ("Foo" ~= "Frog"))
+        )
