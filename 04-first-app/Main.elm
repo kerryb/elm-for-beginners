1304e2226deb0896e146248bcfd5c92341ab8b59
Kerry Buckley <kerryjbuckley@gmail.com>
2018-03-12 15:27:58 +0000

    Set text box value from model

--- 04-first-app/Main.elm
+++ 04-first-app/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Html exposing (Html, beginnerProgram, div, h3, input, button, text)
-import Html.Attributes exposing (type_)
+import Html.Attributes exposing (type_, value)
 import Html.Events exposing (onClick, onInput)
 
 
@@ -52,7 +52,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ h3 [] [ text ("Total calories: " ++ (toString model.total)) ]
-        , input [ type_ "number", onInput UpdateCaloriesToAdd ] []
+        , input [ type_ "number", value (toString model.caloriesToAdd), onInput UpdateCaloriesToAdd ] []
         , button [ type_ "button", onClick (AddCalories) ] [ text "Add" ]
         , button [ type_ "button", onClick Clear ] [ text "Clear" ]
         ]
